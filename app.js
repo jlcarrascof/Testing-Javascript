ae23c446ef18453af40e1e5e40a439801c58deb9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 15:11:52 -0400

    chore: Refactor isVowel function and using assert

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 // Main file ...
-const assert = require('assert')
+const assert = require('assert');
 
-const isVowel = require('./isVowel.test')
+const isVowel = require('./isVowel.test');
 
 console.assert(isVowel('b'), "It's not a vowel");
 console.assert(isVowel('a'), "It's a vowel");
 
-console.log('------------------------');
-console.log(assert);
-console.log('------------------------');
-
-// if (isVowel('b')) {
-//     console.log('Its a vowel');
-// } else {
-//     console.log('Its not a vowel');
-// }
-
-// if (isVowel('a')) {
-//     console.log('Its a vowel');
-// } else {
-//     console.log('Its not a vowel');
-// }
+try {
+    assert.ok(isVowel('e'));
+} catch (error) {
+    console.log(error.message);
+}
