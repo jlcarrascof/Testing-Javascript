6d39f8e21d54fc0b72fe94be59e23a95d7eb8a41
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 13:30:42 -0400

    chore: Add JavaScript code snippets and main file

---
+++ app.js
@@ -0,0 +1,12 @@
+// Main file ...
+const isVowel = require('./isVowel.test')
+
+if (isVowel('b')) {
+    console.log('------------------------');
+    console.log('Its a vowel');
+    console.log('------------------------');
+} else {
+    console.log('------------------------');
+    console.log('Its not a vowel');
+    console.log('------------------------');
+}
