e9b7b516a93b7be89c245f9786356e0a2fe230f0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 13:42:31 -0400

    chore: Update isVowel function test and remove commented code

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Main file ...
 const isVowel = require('./isVowel.test')
 
-if (isVowel('b')) {
-    console.log('Its a vowel');
-} else {
-    console.log('Its not a vowel');
-}
+console.assert(isVowel('b'), "It's not a vowel");
+
+// if (isVowel('b')) {
+//     console.log('Its a vowel');
+// } else {
+//     console.log('Its not a vowel');
+// }
 
 if (isVowel('a')) {
     console.log('Its a vowel');
