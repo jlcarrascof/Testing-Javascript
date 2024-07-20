a20739595cf81dcc906f778cd7ce23887ddf03cc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 13:40:00 -0400

    chore: Remove unnecessary console.log statements in app.js

--- app.js
+++ app.js
@@ -2,21 +2,13 @@
 const isVowel = require('./isVowel.test')
 
 if (isVowel('b')) {
-    console.log('------------------------');
     console.log('Its a vowel');
-    console.log('------------------------');
 } else {
-    console.log('------------------------');
     console.log('Its not a vowel');
-    console.log('------------------------');
 }
 
 if (isVowel('a')) {
-    console.log('------------------------');
     console.log('Its a vowel');
-    console.log('------------------------');
 } else {
-    console.log('------------------------');
     console.log('Its not a vowel');
-    console.log('------------------------');
 }
