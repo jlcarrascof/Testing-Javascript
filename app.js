f912704e087a095ccc180c9ed58e33116f8a50fb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 15:14:33 -0400

    Refactor isVowel function to handle edge case of character not being included in the check

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const assert = require('assert');
 
 const isVowel = require('./isVowel.test');
 
-console.assert(isVowel('b'), "It's not a vowel");
+console.assert(isVowel('e'), "It's not a vowel");
 console.assert(isVowel('a'), "It's a vowel");
 
 try {
     assert.ok(isVowel('e'));
+    assert.equal(isVowel('e'), false)
 } catch (error) {
     console.log(error.message);
 }
