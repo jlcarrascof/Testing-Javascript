fd9dc2acf4fa2b9238c8a10d2d2773c617bdd51f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 14:08:36 -0400

    chore: Refactor isVowel function and remove unnecessary code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // Main file ...
+const assert = require('assert')
+
 const isVowel = require('./isVowel.test')
 
 console.assert(isVowel('b'), "It's not a vowel");
 console.assert(isVowel('a'), "It's a vowel");
+
+console.log('------------------------');
+console.log(assert);
+console.log('------------------------');
 
 // if (isVowel('b')) {
 //     console.log('Its a vowel');
